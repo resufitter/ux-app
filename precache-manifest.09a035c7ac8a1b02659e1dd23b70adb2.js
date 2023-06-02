74f95abe825f0adba0319781c3e58800c8c87277
resufitter3000 <haoranliu@resufitter.com>
2023-06-02 16:41:28 -0400

    Updates

---
+++ precache-manifest.09a035c7ac8a1b02659e1dd23b70adb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed6232498e4eed71faf9",
+    "url": "/ux-app/static/js/main.8364f9d3.chunk.js"
+  },
+  {
+    "revision": "60e2dfca7b136524729a",
+    "url": "/ux-app/static/js/runtime~main.79d06846.js"
+  },
+  {
+    "revision": "5ae6e4135ffa7d8e24ab",
+    "url": "/ux-app/static/js/2.5500bd3a.chunk.js"
+  },
+  {
+    "revision": "481d2397f11bdfcf51ca",
+    "url": "/ux-app/static/js/3.f955e04f.chunk.js"
+  },
+  {
+    "revision": "ea24096632ceb25dd2f1",
+    "url": "/ux-app/static/js/4.b4568eff.chunk.js"
+  },
+  {
+    "revision": "72ececcb15569eb1724aa6a256955540",
+    "url": "/ux-app/index.html"
+  }
+];
